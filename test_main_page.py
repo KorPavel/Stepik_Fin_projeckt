b3ddc74e78c94b3e52437a469757b8890358981f
KorPavel <kor-p@bk.ru>
2022-10-12 00:51:40 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    print('\n\t\tTEST#1 "Guest can go to login page"')
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    print('\n\t\tTEST#1 "Guest can go to login page"')
+    browser.get(link)
+    go_to_login_page(browser)
