7b79b921f0a263ab11f46fc906bdb2e7f1627c4d
KorPavel <kor-p@bk.ru>
2022-10-13 02:08:53 +0300

    Update test_main_page.py
    Теперь тест №1 Проверяет корректность ссылки на страницу Входа/Регистрации; Проверяет наличие формы входа; Проверяет наличие формы регистрации.

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,20 @@
+
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
-# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer", browser.current_url
+
 
 def test_guest_can_go_to_login_page(browser):
     print('\n\t\tTEST#1 "Guest can go to login page"')
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    
 
 def test_guest_should_see_login_link(browser):
     print('\n\t\tTEST#2 "Guest should see login link"')
