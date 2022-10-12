38901c1d0f7630b2953e84c056042ccf82cd2ee7
KorPavel <kor-p@bk.ru>
2022-10-12 22:20:51 +0300

    Update test_main_page.py
    Добавлена новая ссылка с промо-акцией, в качестве примера, чтобы проверить локаторы.

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
 def test_guest_can_go_to_login_page(browser):
     print('\n\t\tTEST#1 "Guest can go to login page"')
