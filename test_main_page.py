81d7d38ff1585746cd2396049afdf2cd85cda434
KorPavel <kor-p@bk.ru>
2022-10-12 18:30:59 +0300

    Update test_main_page.py
    Первый тест на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,9 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
     print('\n\t\tTEST#1 "Guest can go to login page"')
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link) 
+    page.open()
+    page.go_to_login_page()
