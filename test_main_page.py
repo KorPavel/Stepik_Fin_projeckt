75319ba05aab049572f9fc79cb1dc913319f8f7c
KorPavel <kor-p@bk.ru>
2022-10-12 20:15:21 +0300

    Update test_main_page.py
    Добавлен тест, который проверяет, что есть ссылка ведущая на регистрацию.

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) 
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    print('\n\t\tTEST#2 "Guest should see login link"')
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
