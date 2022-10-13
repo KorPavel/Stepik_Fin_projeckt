3e9eb80df99d5d3b00e3c183bccd022992d3bd49
KorPavel <kor-p@bk.ru>
2022-10-13 23:41:37 +0300

    Update locators.py
    Добавлены локаторы для ProductPage.

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,16 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators():
-    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+class LoginPageLocators():   
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
-class LoginPageLocators:
-    LOGIN_FORM = (By.ID, "login_form")
-    REGISTER_FORM = (By.ID, "register_form")
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form > button')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
+    BASKET_MESSAGE = (By.CSS_SELECTOR, '#messages>:first-child>div>strong')
+    BASKET_PRICE = (By.CSS_SELECTOR, '#messages>:last-child>div>p>strong')
