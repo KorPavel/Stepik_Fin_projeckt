256141989bac7c7c9bce77510e7996923e573e83
KorPavel <kor-p@bk.ru>
2022-10-13 23:39:39 +0300

    Update base_page.py
    Добавлен метод "solve_quiz_and_get_code", выполняющий математическую задачу для алерта с выводом результата в консоль.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -12,6 +15,21 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
