818fdadb2ef1813d52a54ba6ccbb898fdca737e2
KorPavel <kor-p@bk.ru>
2022-10-16 00:30:29 +0300

    Update test_main_page.py
    Удалены пробные маркеры

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,6 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-@pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         """Тест-кейс открывает страницу Входа/Регистрации, проверяет наличие элементов
@@ -26,7 +25,6 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()
 
-@pytest.mark.proba
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     '''Тест-кейс открывает страницу корзины, проверяет отсутствие товаров в ней 
        и сообщение о пустой корзине '''
@@ -37,13 +35,4 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_products_in_basket()
     basket_page.message_about_empty_basket_is_presented()
-
-
-
-
-
-
-
-
-
 
