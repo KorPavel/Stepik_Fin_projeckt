54992edf39ab5c52c2baf65ca122ddfc2bbf97bc
KorPavel <kor-p@bk.ru>
2022-10-16 00:37:53 +0300

    Update test_product_page.py
    Создан новый класс с группировкой нескольких тест-кейсов у которых вначале запускается регистрация нового пользователя.
    Расставлены маркеры на тест-кейсы для обязательной проверки.
    Удалён тест-кейс с параметризацией.

--- test_product_page.py
+++ test_product_page.py
@@ -1,54 +1,76 @@
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import pytest
 
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
-params = [x if x != 7 else pytest.param(x, marks=pytest.mark.xfail) for x in range(10)]
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-cathedral-the-bazaar_190/"
 
-@pytest.mark.parametrize('param', params)
-def test_guest_can_add_product_to_basket(browser, param):
-    '''Тест-кейс проверяет ряд промо-кодов, среди которых есть баг, который нужно отметить xfail '''
-    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{param}'
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user()
+        page.should_be_authorized_user()
+    
+    
+    def test_user_cant_see_success_message(self, browser):
+        '''Тест-кейс проверяет, что перед добавлением товара в корзину нет никакого сообщения '''
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        print('\n\t\tTEST# "User cant see success message"')
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
+    
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        '''Тест-кейс проверяет соответствие названия товара и его цены, добавленного в корзину'''
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        print('\n\t\tTEST# "User can add product to basket"')
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_product_to_basket()
+        product_page.compare_basket_and_product_name()
+        product_page.compare_basket_and_product_price()
 
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    '''Тест-кейс проверяет соответствие названия товара и его цены, добавленного в корзину'''
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    print('\n\t\tTEST# "Guest can add product to basket"')
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.compare_basket_and_product_name()
     product_page.compare_basket_and_product_price()
 
-
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    '''Тест-кейс проверяет, что после добавления товара в корзину не должно быть сообщения об этом '''
-    print('\n\t\tTEST#1 "Guest cant see success message after adding product to basket"')
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_product_to_basket()
-    product_page.should_not_be_success_message()
-
-def test_guest_cant_see_success_message(browser):
-    '''Тест-кейс проверяет, что перед добавлением товара в корзину нет никакого сообщения '''
-    print('\n\t\tTEST#2 "Guest cant see success message"')
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
-
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     '''Тест-кейс проверяет, что после добавления товара в корзину сообщение об этом исчезает '''
-    print('\n\t\tTEST#3 "Message disappeared after adding product to basket"')
+    print('\n\t\tTEST# "Message disappeared after adding product to basket"')
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
     product_page.should_be_success_message_disappeared()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    '''Тест-кейс проверяет, что после добавления товара в корзину не должно быть сообщения об этом '''
+    print('\n\t\tTEST# "Guest cant see success message after adding product to basket"')
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_be_success_message()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     '''Тест-кейс открывает страницу корзины, проверяет отсутствие товаров в ней 
        и сообщение о пустой корзине '''
-    print('\n\t\tTEST#4 "Guest cant see product in basket opened from product page"')
+    print('\n\t\tTEST# "Guest cant see product in basket opened from product page"')
     product_page = ProductPage(browser, link) 
     product_page.open()
     product_page.go_to_basket_page()
@@ -56,7 +78,16 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_not_be_products_in_basket()
     basket_page.message_about_empty_basket_is_presented()
 
-
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    """Тест-кейс открывает страницу Входа/Регистрации, проверяет наличие элементов
+       (форма входа, форма регистрации, корректность URL-адреса) """
+    print('\n\t\tTEST# "Guest can go to login page from product page"')
+    page = ProductPage(browser, link) 
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 
