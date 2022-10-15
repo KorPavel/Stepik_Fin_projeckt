8ce5dc013a3a464b01acf1c075001104094223d2
KorPavel <kor-p@bk.ru>
2022-10-15 19:52:25 +0300

    Update test_main_page.py
    Отработан пример группировки двух тест-кейсов в один класс, для удобства запуска этих тестов использована фикстура @pytest.mark.login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -6,24 +6,25 @@ import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
-def test_guest_can_go_to_login_page(browser):
-    """Тест-кейс открывает страницу Входа/Регистрации, проверяет наличие элементов
-       (форма входа, форма регистрации, корректность URL-адреса) """
-    print('\n\t\tTEST#1 "Guest can go to login page"')
-    page = MainPage(browser, link) 
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        """Тест-кейс открывает страницу Входа/Регистрации, проверяет наличие элементов
+           (форма входа, форма регистрации, корректность URL-адреса) """
+        print('\n\t\tTEST#1 "Guest can go to login page"')
+        page = MainPage(browser, link) 
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
     
-
-def test_guest_should_see_login_link(browser):
-    """Тест-кейс проверяет наличие ссылки на страницу Входа/Регистрации """
-    print('\n\t\tTEST#2 "Guest should see login link"')
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    
+    def test_guest_should_see_login_link(self, browser):
+        """Тест-кейс проверяет наличие ссылки на страницу Входа/Регистрации """
+        print('\n\t\tTEST#2 "Guest should see login link"')
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 @pytest.mark.proba
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
