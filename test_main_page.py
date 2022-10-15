985116807aff8dd5ec8a0fc9da9fec2a45f4333d
KorPavel <kor-p@bk.ru>
2022-10-15 18:01:33 +0300

    Update test_main_page.py
    Добавлен тест-кейс для проверки страницы корзины.
    Добавлены подписи к тестам.

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,15 @@
-
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import pytest
 
 
 link = "http://selenium1py.pythonanywhere.com/"
-# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer", browser.current_url
 
 
 def test_guest_can_go_to_login_page(browser):
+    """Тест-кейс открывает страницу Входа/Регистрации, проверяет наличие элементов
+       (форма входа, форма регистрации, корректность URL-адреса) """
     print('\n\t\tTEST#1 "Guest can go to login page"')
     page = MainPage(browser, link) 
     page.open()
@@ -17,7 +19,30 @@ def test_guest_can_go_to_login_page(browser):
     
 
 def test_guest_should_see_login_link(browser):
+    """Тест-кейс проверяет наличие ссылки на страницу Входа/Регистрации """
     print('\n\t\tTEST#2 "Guest should see login link"')
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+@pytest.mark.proba
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    '''Тест-кейс открывает страницу корзины, проверяет отсутствие товаров в ней 
+       и сообщение о пустой корзине '''
+    print('\n\t\tTEST#3 "Guest cant see product in basket opened from main page"')
+    page = MainPage(browser, link) 
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products_in_basket()
+    basket_page.message_about_empty_basket_is_presented()
+
+
+
+
+
+
+
+
+
+
