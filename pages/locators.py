f6e0a05d5216ec20fb5416abca4128bc64bfeb15
KorPavel <kor-p@bk.ru>
2022-10-16 00:41:37 +0300

    Update locators.py
    Добавлен ряд локаторов, необходимых для проведения тестов.

--- pages/locators.py
+++ pages/locators.py
@@ -4,20 +4,19 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a.btn")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators:
     EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
     PRODUCTS_LIST_IN_BASKET = (By.CSS_SELECTOR, "#content_inner > h2")
 
-'''
-Данный класс не обязательный (локаторы в BasePageLocators)
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-'''
-
 class LoginPageLocators:   
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form > button')
