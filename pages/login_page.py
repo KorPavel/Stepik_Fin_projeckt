7000d7c7ed6833597183dd3475dbc893943f780f
KorPavel <kor-p@bk.ru>
2022-10-16 00:43:52 +0300

    Update login_page.py
    Добавлена процедура регистрации нового пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,13 +9,27 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверка на корректный url адрес
         assert "login" in self.browser.current_url, "Incorrect url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "Register form is not presented"
+
+    def register_new_user(self):
+        """ Процедура регистрации пользователя """
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_field.send_keys(self.email)
+        password_1 = self.browser.find_element(*LoginPageLocators.PASSWORD_1)
+        password_1.send_keys(self.password)
+        password_2 = self.browser.find_element(*LoginPageLocators.PASSWORD_2)
+        password_2.send_keys(self.password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
